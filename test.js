05a92e6e8fe6bfac8202ddd191b98c1c01e6840f
Jerry <gisanfu@gmail.com>
2010-12-22 16:52:39 +0800

    fix by multi textarea markdown

--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
 	converter = new Showdown.converter();
 
-	$('textarea').each(function(index){
+	$('.need_markdown').each(function(index){
 		text = converter.makeHtml($(this).text());
 		$('<div id="markdowned' + index + '">' + text + '</div>').insertAfter($(this));
 		$(this).hide();
